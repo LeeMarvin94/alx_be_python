0adcda159c489c0577c73b9709125fec200cac97
LeeMarvin94 <rollymarvinondoabaghe@gmail.com>
2024-06-26 10:06:30 +0100

    Fixing the bug in task 0

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,6 +1,7 @@
 #!/bin/python3
 
 weather = input("What's the weather like today? (sunny/rainy/cold): ")
+
 if weather == 'sunny':
     print('Wear a t-shirt and sunglasses')
 elif weather == 'rainy':
