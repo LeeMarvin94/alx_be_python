d2438c69bdf2730f27affeb17868239f732ed1a2
LeeMarvin94 <rollymarvinondoabaghe@gmail.com>
2024-07-11 15:10:46 +0100

    Adding task 0

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+#python script that contains a class for bank related operations
+
+class BankAccount:
+
+    # construction definition
+    def __init__(self, account_balance = 0):
+        self.account_balance = account_balance
+
+    #method's definition
+    def deposit(self, amount):
+        self.account_balance += amount
+    #method's definition
+    def withdraw(self, amount):
+        if self.account_balance >= amount:
+            account_balance -= amount
+            return True
+        else:
+            return False
+    #method's definition
+    def display_balance(self):
+        print(f'Balance: {self.account_balance}')
+
