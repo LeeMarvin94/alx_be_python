3b32e508f44492e2b49b9460800e44cf3d0a67ee
ONDO ABAGHE Rooly Marvin <rollymarvinondoabaghe@gmail.com>
2025-08-03 14:02:02 +0100

    Commit of the first project

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+#ALX python script for learning purposes
+
+class BankAccount:
+    def __init__(self, account_balance=0 ):
+        self.account_balance = account_balance
+
+    def deposit(self, amount):
+        self.account_balance += amount
+    
+    def withdraw(self, amount):
+        if amount > self.account_balance:
+            return False
+        else:
+            self.account_balance -= amount
+            return True
+        
+    def display_balance(self):
+        print(f"{self.account_balance}")
+
+    
+
+
+
+    
